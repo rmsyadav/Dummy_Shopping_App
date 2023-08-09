e2bcc1f5b6710c375a477c053cad6c9d92495cfd
Ramashankar Kumar <rmsyadav567@gmail.com>
2023-08-10 01:50:25 +0530

    Added protected routing feature

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Suspense, useState } from 'react';
 import {BrowserRouter as Router, Routes,Route} from 'react-router-dom';
 import MyContext from './Store/MyContext';
 import MyErrorBoundary from './Components/UtilityComponents/MyErrorBoundary';
+import Protected from './Components/UtilityComponents/ProtectingRouting';
+import { useSelector } from 'react-redux';
 const Navbar = React.lazy(()=>import("./Components/Navbar"));
 const Carts = React.lazy(()=> import("./Components/Carts"));
 const AboutUs = React.lazy(()=> import("./Components/AboutUs"));
@@ -10,12 +12,12 @@ const Home = React.lazy(()=> import("./Components/Home"));
 const Signup = React.lazy(()=> import('./Components/SignUp'))
 const Signin = React.lazy(()=> import('./Components/SignIn'))
 function App() {
-
+   const {isSignin} = useSelector((state)=>state.userDetails);
    const [contextValue,setContextValue] = useState({
     brandName:""
   })
-
-    
+  
+ 
   return (
     <>
     <MyContext.Provider value={{contextValue,setContextValue}}>
@@ -25,7 +27,13 @@ function App() {
         <Navbar></Navbar>
           <Routes>
             <Route exact path='/' element={<Home></Home>}></Route>
-            <Route exact path='/carts' element={<Carts></Carts>}></Route>
+            <Route exact path='/carts' element=
+              { 
+                 <Protected isSignedIn={isSignin}>
+                  <Carts></Carts>
+                </Protected>
+              }
+            ></Route>
             <Route exact path='/about' element={<AboutUs></AboutUs>}></Route>
             <Route exact path='/signup' element={<Signup></Signup>}></Route>
             <Route exact path='/signin' element={<Signin></Signin>}></Route>
